42e6d484ea171a7e28aa280de0468935193f558d
lalaithion <izaak.weiss@gmail.com>
2015-04-22 17:18:40 -0600

    Markov class added

--- MarkovChain.h
+++ MarkovChain.h
@@ -2,7 +2,19 @@
 #define MARKOVCHAIN_H
 #include "HashTable.h"
 
+class MarkovChain
+{
+private:
+	std::string currentWord;
+	std::string previousWord;
+	void addWord(std::string name);
+	void addEdge(std::string next);
+	HashTable hashTable(10);
+public:
+	MarkovChain(std::string);
+	MarkovChain(std::string filename);
 
+};
 
 
 
