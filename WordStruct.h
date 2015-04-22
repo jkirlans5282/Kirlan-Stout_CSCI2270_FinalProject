d0fcdb1c6eff4ed1654acc7047df927e4c4990ed
jkirlans5282 <jacobkirlanstout@gmail.com>
2015-04-22 16:38:52 -0600

    wordstruct

---
+++ WordStruct.h
@@ -0,0 +1,15 @@
+#ifndef WORDSTRUCT_H
+#define WORDSTRUCT_H
+#import <iostream>
+#include <string>
+struct Word{
+    std::string word;
+    Word *next;
+    Word(){next = NULL; word=""; };
+    Word(std::string in_word)
+    {
+       std:: string word = in_word;
+       next= NULL;
+    }
+};
+#endif // WORDSTRUCT_H
